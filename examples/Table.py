f29a587f93cca7ba572de56272c1f1badb6408f1
William <wkopans123@gmail.com>
2020-09-19 09:05:10 -0400

    Create Table.py

---
+++ examples/Table.py
@@ -0,0 +1,27 @@
+import dash_table
+import plotly.express as px
+from dash import dash
+from gs_quant.session import GsSession, Environment
+from gs_quant.data import Dataset
+from datetime import date
+from project.credentials.config import GSConfig
+
+import numpy as np
+
+import pandas as pd
+
+GsSession.use(client_id=GSConfig.client_id, client_secret=GSConfig.client_secret, scopes=('read_product_data',))
+
+dataset = Dataset('COVID19_SUBDIVISION_DAILY_CDC')
+df = dataset.get_data(countryId='US', start=date(2019, 1, 1))
+
+app = dash.Dash(__name__)
+app.layout = dash_table.DataTable(
+    id='table',
+    columns=[{"name": i, "id": i} for i in df.columns],
+    data=df.to_dict('records'),
+ )
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
