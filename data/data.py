3fef5976ebb5e641b2d5f5044bb6f7b54913d3cb
Lucien Gaitskell <lucieng@gaitskell.com>
2020-09-19 10:22:47 -0400

    Add data loading and caching class

---
+++ data/data.py
@@ -0,0 +1,39 @@
+""" General data structure """
+from abc import ABCMeta, abstractmethod
+import pandas as pd
+from pandas import DataFrame
+from typing import Union
+import os
+from pathlib import Path
+
+PACKAGE_DIR = os.path.dirname(__file__)
+DATA_DIR = os.path.join(PACKAGE_DIR, "bin")
+
+
+class Data(metaclass=ABCMeta):
+    PATH = Path(DATA_DIR)
+    NAME = None
+
+    def __init__(self):
+        self.local_file = self.PATH.joinpath(self.NAME+'.feather')
+
+    @abstractmethod
+    def _remote_load(self) -> DataFrame:
+        """ Load remote data """
+        pass
+
+    def _local_load(self) -> Union[None, DataFrame]:
+        if self.local_file.is_file():
+            return pd.read_feather(self.local_file)
+        return None
+
+    def get(self) -> DataFrame:
+        df = self._local_load()
+        if df is None:
+            df = self._remote_load()
+            df.to_feather(self.local_file)
+        return df
+
+
+if __name__ == "__main__":
+    print("Loading from:", Data.PATH)
