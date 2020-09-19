59a588672edee97ce1f236f0125bd2ad02d5007c
William <wkopans123@gmail.com>
2020-09-19 09:04:40 -0400

    Create CasesPlot.py

---
+++ examples/CasesPlot.py
@@ -0,0 +1,19 @@
+import dash_table
+import plotly.express as px
+from dash import dash
+from gs_quant.session import GsSession, Environment
+from gs_quant.data import Dataset
+from datetime import date
+from project.credentials.config import GSConfig
+
+import numpy as np
+
+import pandas as pd
+
+GsSession.use(client_id=GSConfig.client_id, client_secret=GSConfig.client_secret, scopes=('read_product_data',))
+
+dataset = Dataset('COVID19_SUBDIVISION_DAILY_CDC')
+df = dataset.get_data(countryId='US', start=date(2019, 1, 1))
+
+fig = px.line(df, x=df.index.values, y="totalConfirmed", title='Total Confirmed Over Time')
+fig.show()
