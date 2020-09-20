14652122f2177f251d71b2554645f2c024bf1986
Portia Gaitskell <portia@mit.edu>
2020-09-20 01:08:39 -0400

    Update slider test with variable step

--- examples/dash-opioid-epidemic/slider_test.py
+++ examples/dash-opioid-epidemic/slider_test.py
@@ -12,7 +12,7 @@ from dash.dependencies import Input, Output, State
 
 
 import plotly.express as px
-from datetime import datetime
+from datetime import datetime, timedelta
 from urllib.request import urlopen
 import json
 
@@ -22,42 +22,58 @@ from data.nytimes import NYTQuery
 nytimes = NYTQuery()
 articles = nytimes.get()
 
+state_names = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California",
+               "Colorado", "Connecticut", "District ", "of Columbia", "Delaware", "Florida",
+               "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas",
+               "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan",
+               "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina",
+               "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico",
+               "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania",
+               "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee",
+               "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington",
+               "Wisconsin", "West Virginia", "Wyoming"]
+
+#print(len(state_names))
+
+# step = num days
+def df_step(df, step=10):
+    # get earliest date
+    new_df = pd.DataFrame(columns=df.columns)
+
+    date = (min(df['Date']).to_pydatetime())
+
+    date_increments = [date]
+    while date < max(df['Date']).to_pydatetime():
+        #print(date)
+        new_df = pd.concat([new_df, df.loc[df.Date.eq(date)]])
+        date += timedelta(days=step)
+        date_increments.append(date)
+
+    return new_df, date_increments
+
+def slider_steps(step, date_increments):
+    k = range(0,len(date_increments)*step, step)
+    v = [date_increments[i].strftime('%b %d') for i in range(len(date_increments))]
+
+    return dict(zip(k,v)), dict(zip(k,date_increments))
+
+df = JHCovid().get()
+df = df[(df['Province/State'].isin(state_names))]
+
 PACKAGE_DIR = os.path.dirname(__file__)
 DATA_DIR = os.path.join(PACKAGE_DIR, "data/")
 
-#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
 
 external_stylesheets = ['styles.css']
 
 with urlopen('https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json') as response:
     states = json.load(response)
 
+step = 10
+test_df, date_increments = df_step(df, step)
+slider_marks, value_marks = slider_steps(step, date_increments)
 
-MONTHS = ['FEB', 'MAR', 'APR', 'MAY', 'JUN','JUL','AUG','SEP']
-MONTHS_NUM = [2,3,4,5,6,7,8,9]
-
-DEFAULT_COLORSCALE = [
-    "#f2fffb",
-    "#bbffeb",
-    "#98ffe0",
-    "#79ffd6",
-    "#6df0c8",
-    "#69e7c0",
-    "#59dab2",
-    "#45d0a5",
-    "#31c194",
-    "#2bb489",
-    "#25a27b",
-    "#1e906d",
-    "#188463",
-    "#157658",
-    "#11684d",
-    "#10523e",
-]
-
-slider_marks = dict(zip(MONTHS_NUM,MONTHS))
+#print(slider_marks)
 
 jh = JHCovid()
 df_month = jh.get()
@@ -70,11 +86,11 @@ app.layout = html.Div(
             [
                 dcc.Slider(
                     id='my-slider',
-                    min=2,
-                    max=9,
-                    step=None,
+                    min=min(slider_marks),
+                    max=max(slider_marks),
+                    step=step,
                     marks=slider_marks,
-                    value=3
+                    value=min(slider_marks)
                 ),
             ]
         ),
@@ -120,20 +136,50 @@ app.layout = html.Div(
     className=""
 )
 
+#MONTHS = ['FEB', 'MAR', 'APR', 'MAY', 'JUN','JUL','AUG','SEP']
+#MONTHS_NUM = [2,3,4,5,6,7,8,9]
+
+# DEFAULT_COLORSCALE = [
+#     "#f2fffb",
+#     "#bbffeb",
+#     "#98ffe0",
+#     "#79ffd6",
+#     "#6df0c8",
+#     "#69e7c0",
+#     "#59dab2",
+#     "#45d0a5",
+#     "#31c194",
+#     "#2bb489",
+#     "#25a27b",
+#     "#1e906d",
+#     "#188463",
+#     "#157658",
+#     "#11684d",
+#     "#10523e",
+# ]
+
+#slider_marks = dict(zip(MONTHS_NUM,MONTHS))
+
+#df_month = pd.read_csv(DATA_DIR+'monthly_covid.csv')
+
 
 @app.callback(
     [Output('graph-with-slider', 'figure'),
      Output('nytimes-content', 'children'),],
     [Input('my-slider', 'value')])
-def update_figure(selected_month):
-    #nov_mask = df_month['Dates'].map(lambda x: x.month) == 11
-    filtered_df = df_month[df_month['Date'].map(lambda x: x.month) == selected_month]
-    #filtered_df = df[df.year == selected_month]
+def update_figure(day_increment):
+    # do smallest day, plus day increment to string?
+    min_date = (min(test_df['Date']).to_pydatetime())
+    goal_date = min_date+timedelta(day_increment)
+    filtered_df = test_df[test_df['Date'].map(lambda x: x.to_pydatetime()) == goal_date]
 
-    print(filtered_df)
+    # filtered_df = df_month[df_month['Date'].map(lambda x: datetime.strptime(x,'%Y-%m-%d').month) == selected_month]
+    # filtered_df = df_month[df_month['Date'].map(lambda x: datetime.strptime(x,'%Y-%m-%d').month) == selected_month]
+
+    #print(filtered_df)
 
     fig = px.choropleth_mapbox(filtered_df, geojson=states, locations='FIPS', color='Confirmed',
-                               color_continuous_scale=DEFAULT_COLORSCALE, range_color=(0, 20000),
+                               color_continuous_scale='hot', range_color=(0, 20000),
                                mapbox_style="carto-positron", zoom=2, center={"lat": 37.0902, "lon": -95.7129},
                                hover_name='Confirmed')
 
@@ -143,7 +189,7 @@ def update_figure(selected_month):
 
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0}, transition_duration=500)
 
-    month_articles = articles[articles['date'].map(lambda x: x.month) == selected_month]
+    month_articles = articles[articles['date'].map(lambda x: x.month) == goal_date.month]
     article_html = html.Div([html.A(html.P(c['headline']), href=c['url'], target='_blank') for c in month_articles.iloc])
 
     return (fig, article_html)
